9e02a796d430568c9ca127d6f2173fc2afb45e73
kiran prasad <connectkiran18@gmail.com>
2021-06-03 08:13:57 +0100

    second commit

--- src/Testproject.java
+++ src/Testproject.java
@@ -4,7 +4,7 @@ public class Testproject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	System.out.println("Am learning github");
+	System.out.println("Am learning github1");
 
 	}
 
